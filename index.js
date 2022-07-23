8f4294b7210a68ecbbc22c02b857d464d4020235
akshkin <107682289+akshkin@users.noreply.github.com>
2022-07-23 21:48:35 +0200

    wrote dryer code in js

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const colorOne = document.getElementById("color-name-1")
-const colorTwo = document.getElementById("color-name-2")
-const colorThree = document.getElementById("color-name-3")
-const colorFour = document.getElementById("color-name-4")
-const colorFive = document.getElementById("color-name-5")
-
 
 document.getElementById("get-scheme-btn").addEventListener("click", function(){
 
@@ -13,17 +7,13 @@ document.getElementById("get-scheme-btn").addEventListener("click", function(){
            
       fetch(`https://www.thecolorapi.com/scheme?hex=${seedHex}&mode=${colorMode}`)
         .then(response => response.json())
-        .then(data => {      
-        document.getElementById("color-1").style.backgroundColor = data.colors[0].hex.value
-        colorOne.textContent = data.colors[0].hex.value
-        document.getElementById("color-2").style.backgroundColor = data.colors[1].hex.value
-        colorTwo.textContent = data.colors[1].hex.value
-        document.getElementById("color-3").style.backgroundColor = data.colors[2].hex.value
-        colorThree.textContent = data.colors[2].hex.value
-        document.getElementById("color-4").style.backgroundColor = data.colors[3].hex.value
-        colorFour.textContent = data.colors[3].hex.value
-        document.getElementById("color-5").style.backgroundColor = data.colors[4].hex.value
-        colorFive.textContent = data.colors[4].hex.value 
+        .then(data => {
+          
+          data.colors.forEach((color, index)=>{
+            const hex = color.hex.value
+            document.getElementById(`color-${index+1}`).style.backgroundColor = hex
+            document.getElementById(`color-name-${index+1}`).textContent = hex
+          })      
         
         })
 })
